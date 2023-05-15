58e4094e7f2d68a57efb359435e378f47ca30e4d
JayKolawala <124433284+JayKolawala@users.noreply.github.com>
2023-05-15 19:52:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Header from './components/Header';
+ import TodoForm from './components/TodoForm';
+import Todos from './components/Todos';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TodoForm />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
